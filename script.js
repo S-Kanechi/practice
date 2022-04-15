055913224940866ae737ddea1b5ab65b282e0d0e
S-Kanechi <99860588+S-Kanechi@users.noreply.github.com>
2022-04-15 15:16:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
 "use strict";
 // please do not delete the 'use strict' line above
 
-const button = document
-  .getElementById("color-button")
-  .addEventListener("click", changeColor);
-
 function changeColor() {
   let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   console.log("Button clicked!"); // feel free to change/delete this line
   document.querySelector("body").style.backgroundColor = randomColor;
 }
-
-const musicButton = document.createElement("button");
-musicButton.className = "music";
-musicButton.innerText = "Change the music";
-musicButton.style.width = "500px";
-document.body.appendChild(musicButton);
-musicButton.addEventListener("click", changeMusic);
-
-const playList = [
-  "https://amachamusic.chagasi.com/mp3/yume.mp3",
-  "https://amachamusic.chagasi.com/mp3/seishishitauchu.mp3",
-  "https://amachamusic.chagasi.com/mp3/moodynight.mp3",
-  "https://amachamusic.chagasi.com/mp3/yuubenohoshi.mp3",
-];
-
-function changeMusic() {
-  document.querySelector("iframe").src =
-    playList[Math.floor(Math.random() * 4)];
-}
-
-const languageButton = document.createElement("button");
-languageButton.className = "language";
-languageButton.style.width = "500px";
-languageButton.innerText = "Change the language(言語)";
-document.body.appendChild(languageButton);
-languageButton.addEventListener("click", changeLanguage);
 
 function changeLanguage() {
   if (document.querySelector(".music").innerText === "Change the music") {
@@ -51,11 +21,45 @@ function changeLanguage() {
   }
 }
 
+function changeMusic() {
+  document.querySelector("audio").src =
+    playList[Math.floor(Math.random() * 6)]
+};
+
+
+const button = document
+  .getElementById("color-button")
+  .addEventListener("click", changeColor);
+
+
+const playList = [
+  "https://amachamusic.chagasi.com/mp3/yume.mp3",
+  "https://wingless-seraph.net/material/41_SNES-Battle06.mp3",
+  "https://amachamusic.chagasi.com/mp3/moodynight.mp3",
+  "https://wingless-seraph.net/material/Battle-Demonic.mp3",
+  "https://wingless-seraph.net/material/Battle-Abysswalker.mp3",
+  "https://wingless-seraph.net/material/Battle-Sanctuary.mp3"
+];
+
+const languageButton = document.createElement("button");
+languageButton.className = "language";
+languageButton.style.width = "500px";
+languageButton.innerText = "Change the language(言語)";
+document.querySelector(".operate").appendChild(languageButton);
+languageButton.addEventListener("click", changeLanguage);
+
+const musicButton = document.createElement("button");
+musicButton.className = "music";
+musicButton.innerText = "Change the music";
+musicButton.style.width = "500px";
+document.querySelector(".operate").appendChild(musicButton);
+musicButton.addEventListener("click", changeMusic);
+
 const ericButton = document.createElement("button");
 ericButton.className = "eric";
 ericButton.innerText = "Change The World";
 ericButton.style.width = "500px";
-document.body.appendChild(ericButton);
+document.querySelector(".operate").appendChild(ericButton);
 ericButton.addEventListener("click", function () {
   location.replace("https://www.youtube.com/watch?v=x11NA63gLDM");
-});
+})
